7daf1812e0d2b3a36a677d0855fcad116e9baa8f
Жуль Кирилл <118897479+KirillZhul@users.noreply.github.com>
2023-05-28 16:09:01 +0300

    Update orders_view.sql

--- orders_view.sql
+++ orders_view.sql
@@ -1,18 +1,15 @@
 create or replace view analysis.orders as 
-select 
-	o.order_id,
-	o.order_ts,
-	o.user_id,
-	o.bonus_payment,
-	o.payment,
-	o.cost,
+select o.order_id, 
+	osl.dttm as "order_ts", 
+	o.user_id, 
+	o.bonus_payment, 
+	o.payment, 
+	o."cost", 
 	o.bonus_grant,
-	osl.status_id "status"
-from 
-	production.orders o 
-inner join
-	production.orderstatuslog osl
-		on 1 = 1
-		and o.order_id = osl.order_id 
-		and o.order_ts = osl.dttm 
-where osl.dttm >= '2022-01-01';
+	osl.status_id as "status" 
+from production.orders o
+left join production.orderstatuslog osl 
+on osl.order_id = o.order_id 
+and osl.dttm = (select max(osl2.dttm) 
+from production.orderstatuslog osl2 
+where osl2.order_id = o.order_id);
