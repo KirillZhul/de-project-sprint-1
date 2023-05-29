a28066a83e110760896f68f6c7f5c577a4461ffd
Жуль Кирилл <118897479+KirillZhul@users.noreply.github.com>
2023-05-29 13:21:34 +0300

    Update datamart_query.sql

--- datamart_query.sql
+++ datamart_query.sql
@@ -1,1 +1,9 @@
-
+INCERT INTO analysis.dm_rfm_segments (user_id, recency, frequency, monetary_value)
+SELECT u.id, 
+       r.recency, 
+       f.frequency, 
+       m.monetary_value 
+FROM analysis.users u 
+LEFT JOIN analysis.tmp_rfm_recency r ON u.id = r.user_id 
+LEFT JOIN analysis.tmp_rfm_frequency f ON u.id = f.user_id 
+LEFT JOIN analysis.tmp_rfm_monetary_value m ON u.id = m.user_id;
